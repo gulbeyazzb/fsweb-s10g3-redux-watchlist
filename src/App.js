352d8c966fd7f1c9db0cfac89e2d6247d5ab96a4
Gulbeyaz <glbzbayram@gmail.com>
2023-09-28 13:49:10 +0300

    FavMovies is selected from store

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import { useState } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Movie from "./components/Movie";
 import FavMovie from "./components/FavMovie";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const [sira, setSira] = useState(0);
-  const favMovies = [];
+  const favMovies = useSelector((store) => store.favMovies);
+  const dispatch = useDispatch();
 
   function sonrakiFilm() {
     setSira(sira + 1);
   }
 
+  const addFavListHandler = () => {};
+
   return (
     <div className="wrapper max-w-2xl mx-auto">
       <nav className="flex text-2xl pb-6 pt-8 gap-2 justify-center">
-        <NavLink to="/" exact className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/"
+          exact
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Filmler
         </NavLink>
-        <NavLink to="/listem" className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/listem"
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Listem
         </NavLink>
       </nav>
@@ -32,7 +45,10 @@ function App() {
             >
               Sıradaki
             </button>
-            <button className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white">
+            <button
+              onClick={addFavListHandler}
+              className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white"
+            >
               Listeme ekle
             </button>
           </div>
@@ -40,7 +56,7 @@ function App() {
 
         <Route path="/listem">
           <div>
-            {favMovies.map((movie) => (
+            {favMovies?.map((movie) => (
               <FavMovie key={movie.id} title={movie.title} id={movie.id} />
             ))}
           </div>
