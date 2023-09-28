c57a0fedecbec5706189c034b9d8f581b0df2135
Gulbeyaz <glbzbayram@gmail.com>
2023-09-28 14:14:57 +0300

    middleWare is connected with store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
-import { legacy_createStore as createStore } from "redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import { reducers } from "./reducers";
+import logger from "redux-logger";
+import { movieWare } from "./middleware/favMovieMiddleware";
 
-export const store = createStore(reducers);
+export const store = createStore(reducers, applyMiddleware(movieWare, logger));
