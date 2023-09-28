3a5f2253f21a6bcf138fc9d048e68398c3c5c058
Gulbeyaz <glbzbayram@gmail.com>
2023-09-28 21:34:05 +0300

    middleware is deleted

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,4 @@
-import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import { reducers } from "./reducers";
-import logger from "redux-logger";
-import { movieWare } from "./middleware/favMovieMiddleware";
 
-export const store = createStore(reducers, applyMiddleware(movieWare, logger));
+export const store = createStore(reducers);
