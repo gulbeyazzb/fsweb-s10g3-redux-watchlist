1dd8e4340b2473dc30929be8aa1821e10ec9e89c
Gulbeyaz <glbzbayram@gmail.com>
2023-09-28 14:20:49 +0300

    is editted

--- src/store/actions/favMovieReducerAction.js
+++ src/store/actions/favMovieReducerAction.js
@@ -2,7 +2,7 @@ const ADD_FAVMOVIE = "it adds the film to myList";
 
 const addfavMovieAction = (movie) => {
   return {
-    type: "ADD_FAVMOVIE",
+    type: ADD_FAVMOVIE,
     payload: movie,
   };
 };
