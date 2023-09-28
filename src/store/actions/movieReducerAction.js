778f7b289b0d50f1f9047e8cd9e0aef0c4581fe6
Gulbeyaz <glbzbayram@gmail.com>
2023-09-28 21:33:50 +0300

    movieReducerAction is created

---
+++ src/store/actions/movieReducerAction.js
@@ -0,0 +1,10 @@
+export const NEXT_SİRA = "plus 1 to 'sira' ";
+export const PREV_SİRA = "plus 1 to 'sira' ";
+
+export const nextSiraAction = () => {
+  return { type: NEXT_SİRA };
+};
+
+export const prevSiraAction = () => {
+  return { type: PREV_SİRA };
+};
