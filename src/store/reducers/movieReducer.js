0c83c8b19dbf9841f2b7671b480db725cc2362f4
Gulbeyaz <glbzbayram@gmail.com>
2023-09-28 21:34:47 +0300

    Actions is added at switch case shema

--- src/store/reducers/movieReducer.js
+++ src/store/reducers/movieReducer.js
@@ -1,11 +1,22 @@
 import { movies } from "../../movies";
-const movieInitial = movies;
-
+const movieInitial = {
+  sira: 0,
+  movies: movies,
+};
 const reducer = (state = movieInitial, action) => {
   const { type, payload } = action;
   switch (type) {
-    case "":
-      break;
+    case "NEXT_SİRA":
+      return {
+        ...state,
+        sira: state.sira + 1,
+      };
+
+    case "PREV_SİRA":
+      return {
+        ...state,
+        sira: state.sira - 1,
+      };
 
     default:
       return state;
