1109680d41fe3580d69289ea0ccb82af0a7e5762
Gulbeyaz <glbzbayram@gmail.com>
2023-09-28 14:04:52 +0300

    ADD_FAVMOVIE case is returned at FavMovieReducer

--- src/store/reducers/favMovieReducer.js
+++ src/store/reducers/favMovieReducer.js
@@ -1,11 +1,13 @@
 const favMovieInitial = [];
-const ADD_FAVMOVIE = "it adds the film to myList";
 const reducer = (state = favMovieInitial, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case "ADD_FAVMOVIE":
-      break;
+      return {
+        ...state,
+        payload,
+      };
 
     default:
       return state;
