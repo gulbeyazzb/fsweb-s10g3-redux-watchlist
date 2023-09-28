dada4f8d7cbb2015e53dddd270d585ad20f93cf4
Gulbeyaz <glbzbayram@gmail.com>
2023-09-28 14:03:58 +0300

    favMovieReducerAction is cretated

---
+++ src/store/actions/favMovieReducerAction.js
@@ -0,0 +1,9 @@
+const ADD_FAVMOVIE = "it adds the film to myList";
+
+const addfavMovieAction = (movie) => {
+  return {
+    type: "ADD_FAVMOVIE",
+    payload: movie,
+  };
+};
+export default addfavMovieAction;
